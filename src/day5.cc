a31ad0be35c439f23b50ac0cd44d7fd4583fe52c
Patrik Modesto <mody@centrum.cz>
2023-12-05 15:21:55 +0100

    day 5 - part 2

--- src/day5.cc
+++ src/day5.cc
@@ -9,6 +9,8 @@
 #include <range/v3/view/drop.hpp>
 #include <range/v3/view/transform.hpp>
 
+#include <oneapi/tbb/parallel_for.h>
+
 #include <limits>
 #include <cstdint>
 #include <iostream>
@@ -27,25 +29,49 @@ using Map = std::map<uint64_t, Destination>;
 struct Data {
     std::vector<uint64_t> seeds;
     std::vector<Map> maps;
+
+    uint64_t translate(uint64_t seed) const noexcept;
 };
 
+
+uint64_t Data::translate(uint64_t seed) const noexcept
+{
+    for (auto const& a_map : maps) {
+        for (auto const& [src, dst] : a_map) {
+            if (seed >= src && seed < (src + dst.size)) {
+                seed = (seed - src) + dst.dest;
+                break;
+            }
+        }
+    }
+    return seed;
+}
 
 void part1(Data const& data)
 {
     uint64_t lowest {std::numeric_limits<uint64_t>::max()};
     for (uint64_t seed : data.seeds) {
-        for (auto const& a_map : data.maps) {
-            for (auto const& [src, dst] : a_map) {
-                if (seed >= src && seed < (src + dst.size)) {
-                    seed = (seed - src) + dst.dest;
-                    break;
-                }
-            }
-        }
+        seed = data.translate(seed);
         lowest = std::min(lowest, seed);
     }
 
     fmt::print("1: {}\n", lowest);
+}
+
+void part2(Data const& data)
+{
+    std::atomic<uint64_t> lowest {std::numeric_limits<uint64_t>::max()};
+    for (auto it = data.seeds.begin(); it != data.seeds.end();) {
+        const uint64_t seed {*it++};
+        const uint64_t length {*it++};
+        oneapi::tbb::parallel_for(seed, seed + length, [&lowest, &data](uint64_t _seed) {
+            _seed = data.translate(_seed);
+            uint64_t prev = lowest.load();
+            while (prev > _seed && !lowest.compare_exchange_weak(prev, _seed));
+        });
+    }
+
+    fmt::print("2: {}\n", lowest.load());
 }
 
 int main()
@@ -108,6 +134,7 @@ int main()
     assert(data.maps.size() == 7);
 
     part1(data);
+    part2(data);
 
     return 0;
 }
