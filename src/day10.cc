93fcfaaf4042cd113f184a92049421dc2125f2c8
Patrik Modesto <mody@centrum.cz>
2023-12-10 19:06:27 +0100

    day 10 - part 2

--- src/day10.cc
+++ src/day10.cc
@@ -7,20 +7,13 @@
 #include <iostream>
 #include <map>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 using Coord = int;
 using Point = Gfx_2d::Point<Coord>;
 using Map = std::unordered_map<Point, unsigned char>;
 
-#if 0
-| is a vertical pipe connecting north and south.
-- is a horizontal pipe connecting east and west.
-L is a 90-degree bend connecting north and east.
-J is a 90-degree bend connecting north and west.
-7 is a 90-degree bend connecting south and west.
-F is a 90-degree bend connecting south and east.
-#endif
 
 static const std::map<unsigned char, std::array<Gfx_2d::Direction, 2>> TO_MOVES
 {
@@ -53,6 +46,92 @@ void part1(Map const& map, Point me)
     }
     fmt::print("1: {}\n", steps / 2 + 1);
 }
+
+
+void part2(Map map, Point me)
+{
+    std::unordered_set<Point> path;
+    Point start {me};
+
+    path.insert(start);
+
+    unsigned char last_corner {0};
+    Gfx_2d::Direction last_move{};
+
+    for (Gfx_2d::Direction back {};;) {
+        for (auto dir : TO_MOVES.at(map.at(me))) {
+            if (dir == back) continue;
+            Point next {me + dir};
+            assert(next != me);
+            back = dir.back();
+
+            unsigned char& c = map.at(me);
+
+            if (c == '|') {
+                c = '!';
+                last_corner = ' ';
+            } else if (c == '7' && last_corner == 'L' && last_move == Gfx_2d::Right) {
+                c = '!';
+                last_corner = ' ';
+            } else if (c == 'L' && last_corner == '7' && last_move == Gfx_2d::Left) {
+                c = '!';
+                last_corner = ' ';
+            } else if (c == 'J' && last_corner == 'F' && last_move == Gfx_2d::Right) {
+                c = '!';
+                last_corner = ' ';
+            } else if (c == 'F' && last_corner == 'J' && last_move == Gfx_2d::Left) {
+                c = '!';
+                last_corner = ' ';
+            }
+
+            if (c == 'L' || c == 'F' || c == '7' || c == 'J') last_corner = c;
+
+            last_move = dir;
+            path.insert(me);
+            me = next;
+            break;
+        }
+        if (me == start) {
+            break;
+        }
+    }
+
+    Coord max_x{0}, max_y{0};
+    for (auto const& [px, _] : map) {
+        max_x = std::max(max_x, px.x);
+        max_y = std::max(max_y, px.y);
+    }
+
+    // remove non-path piping
+    for(Coord y = 0; y <= max_y; ++y) {
+        for(Coord x = 0; x <= max_x; ++x) {
+            const Point px {x, y};
+            if (map.contains(px) && !path.contains(px)) {
+                map.erase(px);
+            }
+        }
+    }
+
+    unsigned count{0};
+    for(Coord y = 0; y <= max_y; ++y) {
+        bool inside{false};
+        for(Coord x = 0; x <= max_x; ++x) {
+            const Point px {x, y};
+            auto it = map.find(px);
+            if (it == map.end()) {
+                if (inside) {
+                    map.insert({px, 'I'});
+                    ++count;
+                }
+            } else if (it->second == '!') {
+                inside = !inside;
+            }
+        }
+    }
+
+    fmt::print("2: {}\n", count);
+}
+
 
 int main()
 {
@@ -102,6 +181,7 @@ int main()
     }
 
     part1(map, me);
+    part2(map, me);
 
     return 0;
 }
