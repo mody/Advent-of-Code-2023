a3d462d7b1280ed7b424a806bdd57a6e7e22fb04
Patrik Modesto <mody@centrum.cz>
2023-12-18 21:15:30 +0100

    day 18 - part 1 & 2 using Shoelace Formula and Pick's Theorem

--- src/day18.cc
+++ src/day18.cc
@@ -8,33 +8,52 @@
 
 #include <range/v3/view/drop.hpp>
 
+#include <cassert>
 #include <cmath>
 #include <iostream>
+#include <limits>
 #include <map>
+#include <unordered_set>
 #include <vector>
 
 using Coord = int64_t;
-using Node = std::pair<Gfx_2d::Direction, Coord>;
+using Point = Gfx_2d::Point<Coord>;
+using Points = std::vector<Point>;
+
+int64_t shoelace(Points const& points)
+{
+    using namespace ranges;
+    int64_t sum1{0}, sum2{0};
+
+    Point p1 = points.front();
+    for (Point p2 : points | views::drop(1)) {
+        sum1 += p1.x * p2.y;
+        sum2 += p1.y * p2.x;
+        std::swap(p1, p2);
+    }
+
+    sum1 += p1.x * points.front().y;
+    sum2 += p1.y * points.front().x;
+
+    return std::abs(sum1 - sum2) / 2;
+}
 
 static const std::map<unsigned char, Gfx_2d::Direction> TO_DIRECTION {
     {'R', Gfx_2d::Right}, {'D', Gfx_2d::Down}, {'L', Gfx_2d::Left}, {'U', Gfx_2d::Up},
     {'0', Gfx_2d::Right}, {'1', Gfx_2d::Down}, {'2', Gfx_2d::Left}, {'3', Gfx_2d::Up},
 };
 
-// thanks to https://topaz.github.io/paste/#XQAAAQCWAQAAAAAAAAA4GwhH7u5OdXNRlPeIKYn01V1xhpyceJIwtT5HvB9jA7zH7F67Rry3WbFjXXlg+rj3O3Pcr0jV1/vTXP/7jdZNkrMTQPkbSnuvlXFjzrTfvX3Ir+VxA2REBE39YUc3u64mdAog36DTi6Cd3mk6MAP8HSDqMVD0qJWDIkT/T1frVVjq6Vwl5B9bhr/EfMYhQ76SVYOJ6B7nLXSvqLGiHL5rPpGvcRdh0YoH1xiJatTkING88zdpuFObWwPvuD4cTrcfh/S8kGfa8HfqLQ2orimQG0QsuKI0HONFCDJETbWr//xp3YC3FDnfyWQt6ckdQnNJ6Oha4P/4frRL
-int64_t process(std::vector<Node> const& plan)
-{
-    double pos {0}, result {1};
-    for (auto const& node : plan) {
-        pos += node.first.dx * node.second;
-        result += node.first.dy * node.second * pos + node.second / 2.0;
-    }
-    return result;
-}
-
 int main()
 {
-    std::vector<Node> plan1, plan2;
+    Points points;
+    Point px{0,0};
+    points.push_back(px);
+    Coord total_length {0};
+
+    Points points2;
+    Point px2{0,0};
+    points2.push_back(px2);
+    Coord total_length2 {0};
 
     std::string line;
     while (std::getline(std::cin, line)) {
@@ -43,13 +62,34 @@ int main()
         std::vector<std::string> parts;
         boost::algorithm::split(parts, line, boost::algorithm::is_any_of(" "), boost::token_compress_on);
 
-        plan1.push_back({TO_DIRECTION.at(parts.at(0).at(0)), std::stoull(parts.at(1))});
-        plan2.push_back(
-            {TO_DIRECTION.at(parts.at(2).substr(7, 1).front()), std::stoull(parts.at(2).substr(2, 5), nullptr, 16)});
+        auto dir {TO_DIRECTION.at(parts.at(0).at(0))};
+        int64_t length = std::stoull(parts.at(1));
+
+        dir *= length;
+        px += dir;
+        total_length += length;
+        points.push_back(px);
+
+        dir = TO_DIRECTION.at(parts.at(2).substr(7, 1).front());
+        length = std::stoull(parts.at(2).substr(2, 5), nullptr, 16);
+        dir *= length;
+        px2 += dir;
+        total_length2 += length;
+        points2.push_back(px2);
+
     }
 
-    fmt::print("1: {}\n", process(plan1));
-    fmt::print("2: {}\n", process(plan2));
+    // thanks to https://www.reddit.com/r/adventofcode/comments/18l0qtr/comment/kdveugr/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+    {
+        auto area = shoelace(points);
+        fmt::print("1: {}\n", area + total_length / 2 + 1);
+    }
+
+    {
+        auto area2 = shoelace(points2);
+        fmt::print("2: {}\n", area2 + total_length2 / 2 + 1);
+    }
+
 
     return 0;
 }
